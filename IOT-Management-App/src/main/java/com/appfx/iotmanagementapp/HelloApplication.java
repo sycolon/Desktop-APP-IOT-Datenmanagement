d9b4ced11c88884effacab96cabff39028bf0aa9
Aytun <202412840+code1988xx@users.noreply.github.com>
2025-09-23 12:17:41 +0200

    Fix window size
    Change window size and make it not resizable

--- IOT-Management-App/src/main/java/com/appfx/iotmanagementapp/HelloApplication.java
+++ IOT-Management-App/src/main/java/com/appfx/iotmanagementapp/HelloApplication.java
@@ -15,9 +15,10 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("mainView.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 550, 400);
+        stage.setTitle("Smart-Home-Center");
         stage.setScene(scene);
+        stage.setResizable(false);
         stage.show();
     }
 }
